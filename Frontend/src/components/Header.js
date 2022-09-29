5b7883cb5d9cfec67be1cdc1988b8c41196f4070
ividf <ivandiazfraga@gmail.com>
2022-09-29 20:34:42 +0200

    header

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -1,11 +1,17 @@
 import { NavLink } from "react-router-dom";
 import Auth from "./Auth";
+import logo from "./generatedtext.png";
 
 function Header() {
   return (
     <header>
       <h1>
-        <NavLink to="/">Home aqui ira una imagen</NavLink>
+        <NavLink to="/">
+          <img
+            src={logo}
+            alt="Página no encontrada, haz click aquí para volver a la página principal"
+          />
+        </NavLink>
       </h1>
       <nav>
         <Auth />
